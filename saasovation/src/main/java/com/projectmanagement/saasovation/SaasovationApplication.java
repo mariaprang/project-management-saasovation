fbaf2f5292f978120c16e32300a0d3eb9b887f5f
maria <marie.prangishvili@gmail.com>
2019-09-13 14:46:58 +0200

    Initial commit

---
+++ saasovation/src/main/java/com/projectmanagement/saasovation/SaasovationApplication.java
@@ -0,0 +1,13 @@
+package com.projectmanagement.saasovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasovationApplication.class, args);
+	}
+
+}
---
+++ saasovation/src/test/java/com/projectmanagement/saasovation/SaasovationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projectmanagement.saasovation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaasovationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
