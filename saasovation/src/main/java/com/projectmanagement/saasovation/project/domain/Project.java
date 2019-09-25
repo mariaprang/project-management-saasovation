ca575b548b2a8930ed79208756ebc6c7feb8718e
maria <marie.prangishvili@gmail.com>
2019-09-25 14:00:04 +0200

    Added the ProjectOwner class info, overriden methods in the domain package

--- saasovation/src/main/java/com/projectmanagement/saasovation/project/domain/Project.java
+++ saasovation/src/main/java/com/projectmanagement/saasovation/project/domain/Project.java
@@ -1,18 +1,26 @@
 package com.projectmanagement.saasovation.project.domain;
 
+import com.projectmanagement.saasovation.task.domain.Task;
 import com.projectmanagement.saasovation.team.domain.BaseEntity;
 import com.projectmanagement.saasovation.team.domain.ProjectOwner;
 import com.projectmanagement.saasovation.team.domain.Team;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.Set;
 
+//@Entity
+//@Table(name="project")
 public class Project extends BaseEntity<Long> {
 
-    private String projectName;
-    private ProjectOwner projectOwner;
-    private Team team;
+//    @Column(name = "pr_name", nullable = false, unique = false)
+//    private String projectName;
+//    @Column(name = "first_name", nullable = false, unique = false)
+//    private ProjectOwner projectOwner;
+//    private String projectType;
+//    private Team team;
+//    private Set<Task> tasks;
 
 
 }
--- saasovation/src/main/java/com/projectmanagement/saasovation/team/domain/ProjectOwner.java
+++ saasovation/src/main/java/com/projectmanagement/saasovation/team/domain/ProjectOwner.java
@@ -1,4 +1,57 @@
 package com.projectmanagement.saasovation.team.domain;
 
-public class ProjectOwner {
+import com.projectmanagement.saasovation.project.domain.Project;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "project_owner")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class ProjectOwner extends Member {
+
+    @Transient
+    private Set <Project> projects;
+
+    public ProjectOwner(String firstName, String lastName, String email,
+                        String username, String password, Role role) {
+        super(firstName, lastName, email, username, password, role);
+        projects = new HashSet <>();
+    }
+
+
+    public boolean addProject(Project project) {
+        return projects.add(project);
+    }
+
+    @Override
+    public boolean equals(Object toCompareWith) {
+        boolean isEqual = false;
+        if (toCompareWith != null && this.getClass() == toCompareWith.getClass()) {
+            ProjectOwner typedObject = (ProjectOwner) toCompareWith;
+            isEqual = this.getId().equals(typedObject.getId()) &&
+                    this.getUsername().equals(typedObject.getUsername())
+                    && this.getEmail().equals(typedObject.getEmail());
+        }
+        return isEqual;
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCodeValue =
+                +(71121 * 79)
+                        + this.getId().hashCode()
+                        + this.getUsername().hashCode()
+                        + this.getEmail().hashCode();
+
+        return hashCodeValue;
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectOwner{" + super.toString() + "\n" +
+                "projects=" + projects +
+                '}';
+    }
 }
