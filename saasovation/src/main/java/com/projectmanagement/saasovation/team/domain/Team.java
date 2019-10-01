285229a0be3b838de963e077a9f4bcb51aac7454
maria <marie.prangishvili@gmail.com>
2019-10-01 13:18:16 +0200

    Added a Team model class in the team - domain package

--- saasovation/src/main/java/com/projectmanagement/saasovation/team/domain/Team.java
+++ saasovation/src/main/java/com/projectmanagement/saasovation/team/domain/Team.java
@@ -2,19 +2,55 @@ package com.projectmanagement.saasovation.team.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name="team")
 public class Team extends BaseEntity<Long> {
 
-   // private Set<Member> teamMembers;
+    private Set<Member> teamMembers;
+    private String teamName;
 
-    public Team(){
+    public Team(){ }
 
+    public Team(String teamName){
+        this.teamName = teamName;
+        this.teamMembers = new HashSet <>();
     }
 
-//    public boolean addTeamMember(Member teamMember){
-//        return teamMembers.add(teamMember);
-//    }
+    public boolean addTeamMember(Member teamMember){
+        return teamMembers.add(teamMember);
+    }
+
+    public Set <Member> getTeamMembers() {
+        return teamMembers;
+    }
+
+    public void setTeamMembers(Set <Member> teamMembers) {
+        this.teamMembers = teamMembers;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Team team = (Team) o;
+        return this.getId()==team.getId() && this.teamName.equalsIgnoreCase(team.teamName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), this.getId(), teamName);
+    }
 }
