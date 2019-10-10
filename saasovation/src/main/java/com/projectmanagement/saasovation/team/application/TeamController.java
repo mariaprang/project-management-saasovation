53f24807a3aeb5c8dab7f57149d485e09bfa0d6a
maria <marie.prangishvili@gmail.com>
2019-10-10 12:51:14 +0200

    Added the fragements in the frontend Replaced the big chunks of code in the project.html by adding multiple fragements 'modals' to the fragement package

--- saasovation/src/main/java/com/projectmanagement/saasovation/team/application/TeamController.java
+++ saasovation/src/main/java/com/projectmanagement/saasovation/team/application/TeamController.java
@@ -2,13 +2,18 @@ package com.projectmanagement.saasovation.team.application;
 
 import com.projectmanagement.saasovation.project.domain.Project;
 import com.projectmanagement.saasovation.project.infrastructure.ProjectRepository;
+import com.projectmanagement.saasovation.team.domain.Member;
 import com.projectmanagement.saasovation.team.domain.Team;
 import com.projectmanagement.saasovation.team.infrustructure.repositories.team.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 public class TeamController {
@@ -21,11 +26,21 @@ public class TeamController {
 
 
     @PostMapping("/newTeam/{id}")
-    public String createTeam(@PathVariable("id") long id, @RequestParam("teamName") String teamName){
+    public String createTeam(@PathVariable("id") long id, @RequestParam("teamName") String teamName, Model model){
 
         Project project = projectRepository.findProjectById(id);
         Team team = new Team(teamName, project);
         temTeamRepository.saveTeam(team);
-        return "redirect:/projects/"+id;
+        model.addAttribute("project", project);
+        Set<Member> projectMembers = new HashSet<>();
+        model.addAttribute("teams", project.getTeams());
+
+        for (Team teamchik : project.getTeams()) {
+            for (Member member : teamchik.getTeamMembers()) {
+                projectMembers.add(member);
+            }
+        }
+        model.addAttribute("allMembers", projectMembers);
+        return "project";
     }
 }
